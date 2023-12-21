063530409991a6373389aa46f0ddc14da99d26d0
mukhlesur1001 <mukhlesur1001@gmail.com>
2023-12-22 00:27:33 +0600

    page,profile added

--- app/components/Auth/Login.tsx
+++ app/components/Auth/Login.tsx
@@ -7,6 +7,7 @@ import {FcGoogle} from "react-icons/fc";
 import { styles } from '../Styles/styles';
 import { useLoginMutation } from '@/redux/features/auth/authApi';
 import toast from 'react-hot-toast';
+import { signIn } from 'next-auth/react';
 
 type Props = {
     setRoute:(route:string)=>void;
@@ -120,8 +121,8 @@ const Login:FC<Props>= ({setRoute,setOpen}) => {
                 Or join with
             </h5>
             <div className='flex items-center justify-center my-3'>
-                <FcGoogle size={30} className="cursor-pointer mr-2"/>
-                <AiFillGithub size={30} className="cursor-pointer mr-2 dark:text-white text-black"/>
+                <FcGoogle size={30} className="cursor-pointer mr-2" onClick={()=>signIn("google")}/>
+                <AiFillGithub size={30} className="cursor-pointer mr-2 dark:text-white text-black" onClick={()=>signIn("github")}/>
             </div>
             <h5 className='text-center pt-3 font-Poppins text-[14px] dark:text-white text-black'>
                 Not have any account?{""}
--- redux/features/auth/authSlice.ts
+++ redux/features/auth/authSlice.ts
@@ -1,32 +1,32 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-
 const initialState = {
-    token: "",
-    user: ""
-}
+  token: "",
+  user: "",
+};
 
 const authSlice = createSlice({
-    name:"auth",
-    initialState,
-    reducers:{
-        userRegistration:(state,action: PayloadAction<{token:string}>)=>{
-            state.token = action.payload.token;
-      
-        },
-        userLoggedIn: (state,action: PayloadAction<{accessToken:string,user:string}>)=>{
-            state.token = action.payload.accessToken;
-            state.user= action.payload.user;
+  name: "auth",
+  initialState,
+  reducers: {
+    userRegistration: (state, action: PayloadAction<{ token: string }>) => {
+      state.token = action.payload.token;
+    },
+    userLoggedIn: (
+      state,
+      action: PayloadAction<{ accessToken: string; user: string }>,
+    ) => {
+      state.token = action.payload.accessToken;
+      state.user = action.payload.user;
+    },
+    userLoggedOut: (state) => {
+      state.token = "";
+      state.user = "";
+    },
+  },
+});
 
-        },
-        userLoggedOut: (state)=>{
-            state.token = "";
-            state.user= "";
-
-        }
-    }
-})
-
-export const {userRegistration,userLoggedIn,userLoggedOut}= authSlice.actions;
+export const { userRegistration, userLoggedIn, userLoggedOut } =
+  authSlice.actions;
 
 export default authSlice.reducer;
